ab3bfc01b633f728623a3591ce9b6ca3df51b52c
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2022-09-20 20:58:15 -0300

    Index completo

--- js/main.js
+++ js/main.js
@@ -1,42 +1,27 @@
-const getFileName = () => {
-	let absolutePath = document.URL;
-	let relativePath = absolutePath.substring(absolutePath.lastIndexOf("/") + 1);
-	return relativePath;
-};
-
+/****************************** functions ******************************/
 const getNavbarLinks = () => {
-	navbarLinks.push(new NavbarLink("Inicio", "index.html", "./"));
-	navbarLinks.push(new NavbarLink("Cursos", "cursos.html", "./pages/"));
-	navbarLinks.push(new NavbarLink("El Buda", "buda.html", "./pages/"));
-	navbarLinks.push(new NavbarLink("Viajes", "viajes.html", "./pages/"));
-	navbarLinks.push(new NavbarLink("Nosotros", "nosotros.html", "./pages/"));
-	navbarLinks.push(new NavbarLink("Contacto", "contacto.html", "./pages/"));
+	navbarLinks.push(new NavbarLink("Inicio", "/index.html"));
+	navbarLinks.push(new NavbarLink("Cursos", "/pages/cursos.html"));
+	navbarLinks.push(new NavbarLink("El Buda", "/pages/buda.html"));
+	navbarLinks.push(new NavbarLink("Viajes", "/pages/viajes.html"));
+	navbarLinks.push(new NavbarLink("Nosotros", "/pages/nosotros.html"));
+	navbarLinks.push(new NavbarLink("Contacto", "/pages/contacto.html"));
 };
 
 const renderNavbar = () => {
+	getNavbarLinks();
 	let content = `
         <nav class="navbar navbar-expand-md navbar-dark">
-            <div class="container-fluid position-relative">`;
-	home
-		? (content += `
-                <a class="navbar-brand" href="./index.html"><img src="./img/common/logoMenu.jpg" alt="Logo Atlantida Diving School" height="40px" /></a>`)
-		: (content += `
-                <a class="navbar-brand" href="../index.html"><img src="../img/common/logoMenu.jpg" alt="Logo Atlantida Diving School" height="40px" /></a>`);
-	content += `
+            <div class="container-fluid position-relative">
+                <a class="navbar-brand" href="/index.html"><img src="/img/common/logoMenu.jpg" alt="Logo Atlantida Diving School" height="40px" /></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                     <ul class="navbar-nav">`;
-	if (home) {
-		for (let i = 0; i < navbarLinks.length; i++) {
-			content += `<li class="nav-item"><a class="nav-link px-3" href="${navbarLinks[i].folder}${navbarLinks[i].file}">${navbarLinks[i].label}</a></li>`;
-		}
-	} else {
-		for (let i = 0; i < navbarLinks.length; i++) {
-			i === 0
-				? (content += `<li class="nav-item"><a class="nav-link px-3" href=".${navbarLinks[i].folder}${navbarLinks[i].file}">${navbarLinks[i].label}</a></li>`)
-				: (content += `<li class="nav-item"><a class="nav-link px-3" href="./${navbarLinks[i].file}">${navbarLinks[i].label}</a></li>`);
-		}
-	}
+
+	navbarLinks.forEach((x) => {
+		content += `<li class="nav-item"><a class="nav-link px-3" href="${x.file}">${x.label}</a></li>`;
+	});
+
 	content += `    </ul>
                 </div>
             </div>
@@ -68,15 +53,8 @@ const renderFooter = () => {
 					<p class="mb-0">Tel.: (+54)11.4939.6268 / (+54)11.5979.0203</p>
 					<p class="mb-0">info@divingschool.com.ar</p>
 				</div>
-				<div class="col-3 col-md-2 d-flex justify-content-end align-items-center footer__logoPadi">`;
-
-	home
-		? (content += `
-                	<a href="https://www.padi.com/" target="_blank"><img src="./img/common/logo-padi-transp-black.png" alt="PADI" title="PADI" height="60px" /></a>`)
-		: (content += `
-                	<a href="https://www.padi.com/" target="_blank"><img src="../img/common/logo-padi-transp-black.png" alt="PADI" title="PADI" height="60px" /></a>`);
-
-	content += `	
+				<div class="col-3 col-md-2 d-flex justify-content-end align-items-center footer__logoPadi">
+					<a href="https://www.padi.com/" target="_blank"><img src="/img/common/logo-padi-transp-black.png" alt="PADI" title="PADI" height="60px" /></a>
 				</div>
 			</div>
 			<div class="row">
@@ -85,7 +63,7 @@ const renderFooter = () => {
 			<div class="row">
 				<div class="col pb-5 footer__derechos">
 					<p class="pb-1">
-						Copyright &copy ${anio} - <a href="https://www.instagram.com/nacho.fj/" target="_blank">[ Nacho FJ ]</a> - Todas las fotografías utilizadas en esta página son propiedad exclusiva de Atlantida Diving School, el uso no autorizado de
+						Copyright &copy ${year} - <a href="https://www.instagram.com/nacho.fj/" target="_blank">[ Nacho FJ ]</a> - Todas las fotografías utilizadas en esta página son propiedad exclusiva de Atlantida Diving School, el uso no autorizado de
 						las mismas está penado por la ley.
 					</p>
 				</div>
@@ -95,31 +73,75 @@ const renderFooter = () => {
 	document.getElementsByTagName("footer")[0].innerHTML = content;
 };
 
-const renderWhatsapp = () => {
-	let content = "";
-
-	home
-		? (content += `
-                	<a href="https://api.whatsapp.com/send?phone=5491159790203" target="_blank"><img src="./img/common/logo-whatsapp.png" alt="WhatsApp" /></a>`)
-		: (content += `
-                	<a href="https://api.whatsapp.com/send?phone=5491159790203" target="_blank"><img src="../img/common/logo-whatsapp.png" alt="WhatsApp" /></a>`);
-
+const renderWhatsappIcon = () => {
+	let content = `<a href="https://api.whatsapp.com/send?phone=5491159790203" target="_blank"><img src="/img/common/logo-whatsapp.png" alt="WhatsApp" /></a>`;
 	document.getElementById("whatsapp").innerHTML = content;
 };
 
+const renderDates = () => {
+	let element = document.getElementsByClassName("currentYear");
+	if (element.length > 0) {
+		for (let i = 0; i < element.length; i++) {
+			element[i].innerHTML = year;
+		}
+	}
+
+	element = document.getElementsByClassName("yearsSince1986");
+	if (element.length > 0) {
+		for (let i = 0; i < element.length; i++) {
+			element[i].innerHTML = year - 1986;
+		}
+	}
+};
+
+const getImages = () => {
+	images.push(new Image("/img/viajes/202206-angra-dos-reis.jpg", "Angra Dos Reis Brasil"));
+	images.push(new Image("/img/viajes/202204-ilha-bela.jpg", "Ilha Bela Brasil"));
+	images.push(new Image("/img/viajes/202203-cantera-ezeiza.jpg", "Cantera Ezeiza Argentina"));
+	images.push(new Image("/img/viajes/202112-cantera-tandil-2.jpg", "Cantera Tandil Argentina"));
+	images.push(new Image("/img/viajes/202112-cantera-tandil-1.jpg", "Cantera Tandil Argentina"));
+	images.push(new Image("/img/viajes/202112-angra-dos-reis.jpg", "Angra Dos Reis Brasil"));
+};
+
+const renderHomeViajes = () => {
+	getImages();
+	let content = "";
+	let limitFor = 0;
+	images.length >= 6 ? (limitFor = 6) : (limitFor = images.length);
+	for (let i = 0; i < limitFor; i++) {
+		content += `
+			<div class="col-sm-6 col-lg-4 p-3">
+				<img src="${images[i].src}" alt="${images[i].alt}" />
+			</div>
+			`;
+	}
+	homeViajes.innerHTML = content;
+};
+
+/****************************** main ******************************/
 class NavbarLink {
-	constructor(label, file, folder) {
+	constructor(label, file) {
 		this.label = label;
 		this.file = file;
-		this.folder = folder;
+	}
+}
+class Image {
+	constructor(src, alt) {
+		this.src = src;
+		this.alt = alt;
 	}
 }
 
 const navbarLinks = [];
-let home = getFileName() === "index.html";
-const anio = new Date().getFullYear();
+const images = [];
+const year = new Date().getFullYear();
 
-getNavbarLinks();
 renderNavbar();
 renderFooter();
-renderWhatsapp();
+renderWhatsappIcon();
+renderDates();
+
+const homeViajes = document.getElementById("homeViajes");
+if (homeViajes !== null) {
+	renderHomeViajes();
+}
